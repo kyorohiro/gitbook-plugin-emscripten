dbac0c869d4dcc23790bfbdbf106102ffaabf516
kyorohiro <kyorohiro@kyorohironoMac-mini.local>
2017-08-31 20:29:04 +0900

    ..

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ module.exports = {
       emscripten: {
         process: function(block) {
 
-          return
-          '<div class="emscripten_border">'
+          return '<div class="emscripten_border">'
           +'<canvas class="emscripten" id="canvas" oncontextmenu="event.preventDefault()"></canvas>'
           +block.kwargs.js+'</div>'
           +'<textarea id="output" rows="8"></textarea>'

589775f3514887c2cfcdb5dd9c6cfda3644888c2
kyorohiro <kyorohiro@kyorohironoMac-mini.local>
2017-08-31 20:39:19 +0900

    ..

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ module.exports = {
   blocks: {
       emscripten: {
         process: function(block) {
-
-          return '<div class="emscripten_border">'
+          return '<div class="spinner" id="spinner"></div>'
+          +'<div class="emscripten" id="status">Downloading...</div>'
+          +'<div class="emscripten">'
+          +'  <progress value="0" max="100" id="progress" hidden=1></progress>'
+          +'</div>'
+          +'<div class="emscripten_border">'
           +'<canvas class="emscripten" id="canvas" oncontextmenu="event.preventDefault()"></canvas>'
           +block.kwargs.js+'</div>'
           +'<textarea id="output" rows="8"></textarea>'
